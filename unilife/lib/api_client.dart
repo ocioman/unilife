ff6973d8d9a870fe906a8094f9744677f2d928db
ocioman <120775542+ocioman@users.noreply.github.com>
2026-02-21 02:54:07 +0100

    fetchExams e fetchGrades

--- unilife/lib/api_client.dart
+++ unilife/lib/api_client.dart
@@ -1,12 +1,15 @@
-import 'package:flutter/cupertino.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:unilife/model/user_model.dart';
+
+import 'model/exam.dart';
+import 'model/grade.dart';
 
 
 //TODO: sicurezza password e correttezza email
 
 class ApiClient{
   final SupabaseClient _supabase=Supabase.instance.client;
+  late String currUuid;
 
   Future<void> signUp({required String email, required String password, required String nome1, String? nome2, required String cognome1, String? cognome2,})async {
     try {
@@ -20,7 +23,7 @@ class ApiClient{
           'second_surname': cognome2,
         },
       );
-    } on AuthException catch (e) { //eccezione causata da supabase (password, email ecc...)
+    } on AuthException { //eccezione causata da supabase (password, email ecc...)
       rethrow; //rilancio l'eccezione alla UI così la gestisco con una snackbar
     }catch(e){ //eccezione causata NON da supabase (tipo se non sono connesso ad internet ottengo una SocketException)
       rethrow;
@@ -49,8 +52,40 @@ class ApiClient{
               .eq('userID', uuid)
               .single();
 
+          currUuid=res.user!.id; //evito il join nelle query per i fetch
+          //uso il bang perché a questo punto sono sicuro che user non sia null, altrimenti avrei già catturato l'eccezione
           return UserModel.fromJson(resJson);
-    }on AuthException catch(e){
+    }on AuthException{
+      rethrow;
+    }catch(e){
+      rethrow;
+    }
+  }
+
+  Future<List<Exam>> fetchExams() async{
+    try{
+        List<dynamic> resJson=await _supabase
+            .from('exams')
+            .select()
+            .eq('userID', currUuid);
+
+        return resJson.map((examJson)=>Exam.fromJson(examJson)).toList();
+    }on AuthException{
+      rethrow;
+    }catch(e){
+      rethrow;
+    }
+  }
+
+  Future<List<Grade>> fetchGrades() async{
+    try{
+      List<dynamic> resJson=await _supabase
+          .from('grades')
+          .select()
+          .eq('userID', currUuid);
+
+      return resJson.map((gradesJson)=>Grade.fromJson(gradesJson)).toList();
+    }on AuthException{
       rethrow;
     }catch(e){
       rethrow;
