702e8c0ac09cc7b70e7e4dbc1a2030bccd6da1e9
ocioman <120775542+ocioman@users.noreply.github.com>
2026-02-24 17:07:29 +0100

    Globalizzato api client, iniziata la pagina di login, aggiunti i widget shadcn

---
+++ unilife/lib/login_page.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/cupertino.dart';
+
+class LoginPage extends StatefulWidget{
+  const LoginPage({super.key});
+
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    throw UnimplementedError();
+  }
+}
